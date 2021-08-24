0debe4a4c906fc40e776ae8bc15d4e04642bcae4
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-24 11:27:53 +0530

    using switch case

--- Empwage.java
+++ Empwage.java
@@ -3,15 +3,18 @@ public class Empwage {
         System.out.println("Welcome to Employee wage computution program");
         double a = Math.floor(Math.random() * 10) % 3;
         int empHrs = 0;
-        if (a == 0) {
-            System.out.println("Employee is Absent");
-            empHrs = 0;
-        } else if(a==1) {
-            System.out.println("Employee is Present");
-            empHrs = 8;
-        }else if(a==2){
-            System.out.println("Employee doing Part Time");
-            empHrs = 3;
+        switch ((int) a) {
+            case 0:
+                System.out.println("Employee is Absent");
+                empHrs = 0;
+                break;
+            case 1:
+                System.out.println("Employee is Present");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee doing Part Time");
+                empHrs = 3;
         }
         int emp_rate = 20;
         int empwage = empHrs * emp_rate;
