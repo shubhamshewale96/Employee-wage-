f4546695fe919db97da35c92108fd2f2375e4bbc
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-24 11:52:08 +0530

    Calculate Wages till

--- Empwage.java
+++ Empwage.java
@@ -1,9 +1,11 @@
 public class Empwage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computution program");
-        int empHrs = 0, Workday = 20, emp_rate = 20, totempwage = 0;
-        for (int day = 1; day <= Workday; day++) {
+        int empHrs = 0, emp_rate = 20, totemphrs = 0, totworkday = 0;
+
+        while (totemphrs <= 100 && totworkday < 20) {
             int a = (int) Math.floor(Math.random() * 10) % 3;
+            totworkday++;
             switch (a) {
                 case 0:
                     System.out.println("Employee is Absent");
@@ -17,12 +19,12 @@ public class Empwage {
                     System.out.println("Employee doing Part Time");
                     empHrs = 3;
             }
-
-            int empwage = empHrs * emp_rate;
-            totempwage += empwage;
-            System.out.println("Day "+day+" Employee Wage is Rs." + empwage);
+            totemphrs += empHrs;
+            System.out.println("Day " + totworkday + " Employee Hours: " + empHrs);
         }
-        System.out.println("Total Employee wage for a Month Rs. " + totempwage);
+        int totempwag = totemphrs * emp_rate;
+        System.out.println("Total Working Hours: " + totemphrs);
+        System.out.println("Total Employee wage for a Month Rs. " + totempwag);
     }
 }
 
