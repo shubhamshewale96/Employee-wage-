e72360659e6abea022505da0b9352bd89bcd8e30
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-24 11:15:00 +0530

    adding part time employee wage

--- Empwage.java
+++ Empwage.java
@@ -1,14 +1,17 @@
 public class Empwage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computution program");
-        double a = Math.floor(Math.random() * 10) % 2;
+        double a = Math.floor(Math.random() * 10) % 3;
         int empHrs = 0;
         if (a == 0) {
             System.out.println("Employee is Absent");
             empHrs = 0;
-        } else {
+        } else if(a==1) {
             System.out.println("Employee is Present");
             empHrs = 8;
+        }else if(a==2){
+            System.out.println("Employee doing Part Time");
+            empHrs = 3;
         }
         int emp_rate = 20;
         int empwage = empHrs * emp_rate;
