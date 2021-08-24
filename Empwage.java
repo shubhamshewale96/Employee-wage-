cbfe00721fa594e4200508304351e585217d8743
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-24 11:43:32 +0530

    calculate wage for month

--- Empwage.java
+++ Empwage.java
@@ -1,24 +1,28 @@
 public class Empwage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computution program");
-        double a = Math.floor(Math.random() * 10) % 3;
-        int empHrs = 0;
-        switch ((int) a) {
-            case 0:
-                System.out.println("Employee is Absent");
-                empHrs = 0;
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                empHrs = 8;
-                break;
-            default:
-                System.out.println("Employee doing Part Time");
-                empHrs = 3;
+        int empHrs = 0, Workday = 20, emp_rate = 20, totempwage = 0;
+        for (int day = 1; day <= Workday; day++) {
+            int a = (int) Math.floor(Math.random() * 10) % 3;
+            switch (a) {
+                case 0:
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+                    break;
+                case 1:
+                    System.out.println("Employee is Present");
+                    empHrs = 8;
+                    break;
+                default:
+                    System.out.println("Employee doing Part Time");
+                    empHrs = 3;
+            }
+
+            int empwage = empHrs * emp_rate;
+            totempwage += empwage;
+            System.out.println("Day "+day+" Employee Wage is Rs." + empwage);
         }
-        int emp_rate = 20;
-        int empwage = empHrs * emp_rate;
-        System.out.println("Employee Wage is Rs."+empwage);
+        System.out.println("Total Employee wage for a Month Rs. " + totempwage);
     }
 }
 
