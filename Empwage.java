a64fe4cedb2f4e14e93ce68be5aeb71e63aa434a
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-24 11:01:30 +0530

    calculate daily employee wage

--- Empwage.java
+++ Empwage.java
@@ -1,11 +1,18 @@
 public class Empwage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computution program");
-        double a = Math.floor(Math.random()*10)%2;
-        if (a==0){
+        double a = Math.floor(Math.random() * 10) % 2;
+        int empHrs = 0;
+        if (a == 0) {
             System.out.println("Employee is Absent");
-        }else
+            empHrs = 0;
+        } else {
             System.out.println("Employee is Present");
+            empHrs = 8;
+        }
+        int emp_rate = 20;
+        int empwage = empHrs * emp_rate;
+        System.out.println("Employee Wage is Rs."+empwage);
     }
 }
 
